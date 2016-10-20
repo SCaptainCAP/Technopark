57f4eb265d5fd7c085198cd4890fe61e105ca85e
SCaptaincAP <sgv123@yandex.ru>
2016-10-20 20:39:17 +0300

    Laba 6 is working

--- Technopark_Algoritms/Laba6_A/main.cpp
+++ Technopark_Algoritms/Laba6_A/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <assert.h>
 #include <cstring>
 /*Дано N кубиков. Требуется определить каким количеством способов можно выстроить из этих кубиков пирамиду.
 
@@ -17,48 +16,54 @@ int min(int a, int b){
     return (a < b) ? a : b;
 }
 
-int calculate_layer(int to_add, int level)
+long int calculate_layer(int to_add, int level, long int **buffer, int depth)
 {
-    std::cout << "To add: " <<  to_add << ", level: " << level << std::endl;
 
     if (to_add <= 1)
         return 1;
     if (level == 1)
         return 1;
-    int ans = 0;
-    int razn = (level < to_add) ? (to_add - level) : 0;
-    //if (to_add <= level)
+
+    if (buffer[to_add][level] != -1)
+        return buffer[to_add][level];
+
+    long int ans = 0;
+    int razn = (to_add > level) ? (to_add - level) : 0;
     {
         for (int i = 0; i < min(to_add, level); i++){
-            ans += calculate_layer(i + razn, to_add - i - razn);
-            //std::cout << "Ans: " <<  ans << std::endl;
-
+            buffer[i + razn][to_add - i - razn] = calculate_layer(i + razn, to_add - i - razn, buffer, depth + 1);
+            ans += buffer[i + razn][to_add - i - razn];
         }
     }
-//    for (int i = 0; i < to_add - level; i++) {
-//        ans += calculate_layer(i + level - to_add, level);
-//        std::cout << "Ans: " <<  ans << std::endl;
-//    }
 
 
     return ans;
 }
 
+long int find(int n)
+{
+    long int ans = 0;
+    long int **buffer = new long int*[n + 1];
+    for(int i = 0; i < n + 1; i++) {
+        buffer[i] = new long int[n + 1];
+        for (int j = 0; j < n + 1 ; j++) {
+            buffer[i][j] = -1;
+        }
+    }
 
+    ans = calculate_layer(n, n, buffer, 0);
+
+    for(int i = 0; i < n; ++i) {
+        delete [] buffer[i];
+    }
+    delete [] buffer;
+    return ans;
+}
 int main()
 {
     int n = 0;
-    int ans = 0;
     std::cin >> n;
 
-//    for (int i = 1; i <= n; i++)
-//        ans += calculate_layer(n - i, i);
-//    std::cout << calculate_layer(3, 3) << std::endl << std::endl;
-//    std::cout << calculate_layer(4, 4) << std::endl << std::endl;
-//    std::cout << calculate_layer(5, 5) << std::endl << std::endl;
-//    std::cout << calculate_layer(7, 7) << std::endl << std::endl;
-    std::cout << calculate_layer(n, n);
+    std::cout << find(n);
     return 0;
 }
-
-///200­(123+34*2)+(48­2)   =   5
