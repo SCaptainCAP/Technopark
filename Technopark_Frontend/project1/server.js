8fbcb12b928959a29627195bf1d2dbd23b9a2873
SCaptainCAP <SGV123@yandex.ru>
2017-02-16 20:34:13 +0300

    Start HTML design for frontend

--- Technopark_Frontend/project1/server.js
+++ Technopark_Frontend/project1/server.js
@@ -4,10 +4,17 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function(req, resp){
+
+    const url = req.url;
+    console.log(url);
+
     const text = fs.readFileSync('./static/index.html', 'utf8');
     resp.write(text);
     resp.end();
 });
 
-server.listen(3000);
-console.log('kek');
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT);
+console.log(PORT);
